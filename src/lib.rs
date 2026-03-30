7e008576a40a79dc5ed51532d522a71c431b9388
Jonah Ringdahl <46978061+JonahRingdahl@users.noreply.github.com>
2026-03-30 13:21:14 -0400

    remove dup route

--- src/lib.rs
+++ src/lib.rs
@@ -27,7 +27,7 @@ pub fn App() -> impl IntoView {
         <Meta charset="UTF-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-        <Router base="/blog">
+        <Router>
             <Routes fallback=|| view! { NotFound }>
                 <Route path=path!("/") view=Home />
                 <Route path=path!("/articles") view=Articles />
